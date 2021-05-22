24fd6be151ecfd65868e8ee1afe227af7796a531
Apple <sdjee2015@gmail.com>
2021-05-22 23:34:49 +0530

    Trying to fight babel

--- config/database.js
+++ config/database.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 class Connection {
   constructor() {
-    const url = process.env.MONGODB_URI || '';
+    const url =
+      process.env.MONGODB_URI ||
+      'mongodb+srv://saransh:Saransh@321@cluster0.chqmt.mongodb.net/test';
     mongoose.Promise = global.Promise;
     mongoose.set('useNewUrlParser', true);
     mongoose.set('useFindAndModify', false);
