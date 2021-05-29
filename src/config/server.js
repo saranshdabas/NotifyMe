8818ed30b86aa774a82834aaab39b2ad31b8a981
Apple <sdjee2015@gmail.com>
2021-05-30 00:20:04 +0530

    Minor change

--- src/config/server.js
+++ src/config/server.js
@@ -5,6 +5,10 @@ import setRoutes from './routes';
 const server = express();
 server.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
+  res.headers(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, DELETE'
+  );
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
