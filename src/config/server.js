7e84f1733bd7127580f34a3afb1c054739b7bbca
Apple <sdjee2015@gmail.com>
2021-05-30 00:40:10 +0530

    Fighting CORS, need to fix it later with a better soln

--- src/config/server.js
+++ src/config/server.js
@@ -1,17 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import setRoutes from './routes';
+import cors from 'cors';
 
 const server = express();
-server.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+server.use(cors);
+// server.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept'
+//   );
+//   next();
+// });
 server.use(bodyParser.json());
 setRoutes(server);
 
