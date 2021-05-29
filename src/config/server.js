a69448e7fad4ccac2681374b49bd45b2e6391b51
Apple <sdjee2015@gmail.com>
2021-05-30 00:50:28 +0530

    Minor change

--- src/config/server.js
+++ src/config/server.js
@@ -4,16 +4,16 @@ import setRoutes from './routes';
 import cors from 'cors';
 
 const server = express();
-server.use(cors);
-// server.use(function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept'
-//   );
-//   next();
-// });
+server.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+server.options('*', cors());
 server.use(bodyParser.json());
 setRoutes(server);
 
